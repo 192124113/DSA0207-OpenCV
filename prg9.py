fb0996c29efc098f46e7048c86132631e2d569e9
192124113 <113336960+192124113@users.noreply.github.com>
2023-10-16 12:15:55 +0530

    output

---
+++ prg9.py
@@ -0,0 +1,7 @@
+import cv2
+path ="C:/Users/91956/Downloads/land/GDL/class 1/download (16).png"
+src = cv2.imread(path)
+window_name = 'Image'
+image = cv2.rotate(src, cv2.ROTATE_180)
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
